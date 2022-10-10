04fc2be81e66fd682494f7b59e0049c5e9a25005
karume <karumekate8@gmail.com>
2022-10-10 14:00:33 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const p = document.getElementById("text");
+    p.textContent = "This is really cool!";
+  });
+  Footer
+  document.addEventListener("DOMContentLoaded", function () {
+    const p = document.getElementById("text");
+    p.textContent = "God save the queen";
+  });
+  Footer
+  
